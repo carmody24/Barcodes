3a2457a043030a86e82fad3e444c693e11f9f3a1
carmody24 <carmodycaitlin8@gmail.com>
2025-02-20 10:22:55 -0800

    copy from separate folder CCS3Optional_No_Tile_Control_Map3_BASH.sh

---
+++ Barcode1_Look_Up_Table/Combined/CCS3Optional_No_Tile_Control_Map3_BASH.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=No_Tile_Map3
+#SBATCH --account=
+#SBATCH --partition=savio3_bigmem
+#SBATCH --time=10:00:00
+#SBATCH --output=%x-%j.out   # Save output to jobname-jobid.out file
+#SBATCH --error=%x-%j.err    # Save error to jobname-jobid.err file
+
+# Load necessary modules
+module load python
+
+# Install pandas if not already available
+pip install --user pandas seaborn
+
+# Check if the necessary arguments are provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <input_directory>"
+  exit 1
+fi
+
+# Get the input directory from the command line arguments
+INPUT_DIR=$1
+
+# Call your Python script with the necessary parameters
+python3 /pathway/to/CCS3Optional_No_Tile_Control_Map3.py --input_dir "$INPUT_DIR"
+
+echo "Processing complete for replicates in $INPUT_DIR."
